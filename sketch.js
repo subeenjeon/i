34d445661d45d7de7954dc0524480d46b4ca35b5
subeenjeon <powersubeen@naver.com>
2014-09-18 16:21:13 +0900

    d
    d

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+	createCanvas(640, 400);
+	
+	backgroundColor = color(0, 0, 255);
+	fillColor = color(0, 0, 255);
+}
+
+function draw() {
+	background(backgroundColor);
+	
+	fillColor = color(
+		Math. random() *255,
+		Math. random() *255, 
+		Math. random() *255, 
+		Math. random() *255);
+
+	mouseColor = color(
+		mouseX / 640 * 128 + Math. random() * 128,
+		mouseY / 480 * 255, 0);
+
+// fill(fillColor);
+// rect(100, 100, 100, 100);
+	fill(mouseColor);
+	ellipse(150,150,200,200);
+
+	fill(fillColor);
+	rect(100, 100, 100, 100);
+}
